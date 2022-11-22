01158fb93f6b0e9ea3fb7d0fa0a0e1a066fb8263
viktorlov <orlovva@ymail.com>
2022-11-22 20:31:37 +0300

    initial fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+cache = {0: 0, 1: 1}
+
+
+def fibonacci_of(n):
+    if n in cache:
+        return cache[n]
+    cache[n] = fibonacci_of(n - 1) + fibonacci_of(n - 2)
+    return cache[n]
+
+
+print(*[fibonacci_of(n) for n in range(1, n + 1)], sep=" ")
